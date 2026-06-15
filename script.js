e888ae4c5ba7c23c74763ede30dfa5530f429264
veon <veon@vp.pl>
2026-06-15 12:24:01 +0200

    budget-calculator

---
+++ script.js
@@ -0,0 +1,37 @@
+const totalamount = document.getElementById("total-amount");
+const setBudgetButton = document.getElementById("set-budget");
+const budgetp = document.getElementById("budget");
+const itemName = document.getElementById("title");
+const costProduct = document.getElementById("cost");
+const checkamountbutton = document.getElementById("check-amount");
+const list = document.getElementById("list");
+const errorMessage = document.getElementById("error-message");
+
+const produkty = [];
+
+function setBudget() {
+  console.log("test");
+  const budgetValue = Number(totalamount.value);
+  budgetp.innerHTML = "budżet: " + budgetValue + "zł";
+  totalamount.value = "";
+}
+setBudgetButton.addEventListener("click", setBudget);
+function addItem() {
+  const nameItem = itemName.value;
+  const cost = costProduct.value;
+  if (nameItem === "" || cost === "") {
+    errorMessage.textContent =
+      "Please complete both fields before adding a product!";
+    errorMessage.className = "error-text";
+    return;
+  }
+  errorMessage.textContent = "";
+  produkty.push({ nazwa: nameItem, cena: cost });
+  console.log(produkty);
+  const newElement = document.createElement("li");
+  newElement.textContent = nameItem + " - " + cost + "zł";
+  list.appendChild(newElement);
+  itemName.value = "";
+  costProduct.value = "";
+}
+checkamountbutton.addEventListener("click", addItem);
